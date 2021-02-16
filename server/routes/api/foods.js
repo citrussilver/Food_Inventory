82773c676ae4fb7eb0b914f0feacb497adc52c8d
Hisokage <sengoku.zange@gmail.com>
2021-02-16 12:42:07 +0800

    Refactor code

--- server/routes/api/foods.js
+++ server/routes/api/foods.js
@@ -9,49 +9,24 @@ router.get('/', async(req, res) =>{
         const food = await FoodItem.find() 
             .populate('ingredients')
             .exec();
-        res.json(food);
+        res.status(200).json(food);
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 });
 
 router.patch('/:ingredientId', async(req, res) =>{
-    // res.send(req.params);
-    // res.send("Server:\nFood ID is: " + req.body.foodId +"\nIngredient ID is: " + req.params.ingredientId + "\nOn Hand Qty is: " + req.body.onHandQty);
-    const ingredientId = req.params.ingredientId;
-    
+    const ingredientId = req.params.ingredientId;    
     const onHandQty = req.body.onHandQty;
     const ingredientName = req.body.ingredientName;
-    const ingredient = await IngredientItem.updateOne({ _id: ingredientId }, { onHandQty: onHandQty }, function(err){
-        if(err) {
-            res.send(err);
-        } else {
-            res.send("[Ingredients] Successfully updated " + ingredientName + " quantity.");
-        }
-    });
+
+    try {
+        const ingredient = await IngredientItem.updateOne({ _id: ingredientId }, { onHandQty: onHandQty })
+        if (!ingredient.nModified) throw Error('Something went wrong ')
+        res.status(200).send("[Ingredients] Successfully updated " + ingredientName + " quantity.")
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 });
-
-// router.post('/', async(req, res) => {
-//     const newfood = new FoodItem(req.body)
-
-//     try {
-//         const food = await newfood.save();
-//         if(!food) throw new Error('Something went wrong saving the food')
-//         res.status(200).json(food);
-//     } catch (error) {
-//         res.status(500).json({ message: error.message})
-//     }
-// })
-
-// router.delete('/:id', async (req, res) => {
-//     const { id } = req.params
-//     try {
-//         const removed = await FoodItem.findByIdAndDelete(id)
-//         if (!removed) throw Error('Something went wrong ')
-//         res.status(200).json(removed)
-//     } catch (error) {
-//         res.status(500).json({ message: error.message })
-//     }
-// })
 
 module.exports = router
